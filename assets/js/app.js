5f43fbeac77da9a750ed000bef469e2bc30367eb
juanCano29 <46565353+juanCano29@users.noreply.github.com>
2022-10-17 15:57:53 -0500

    paginado

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,28 +2,35 @@ new Vue({
   el: '#buscar',
   data: {
     name: "",
-    inputImdbID: '',
-    inputTitle: '',
-    inputType: '',
-    inputYear: '',
+    inputImdbID: "",
+    inputTitle: "",
+    inputType: "",
+    inputYear: "",
+    elementosPorPagina: 10,
+    resultadoCantidad: 0,
+    paginaActual: 1,
+    datosPaginados: [],
     peliculas: [],
     peliculasDB: []
   },
   mounted() {
-    this.llenarTablaDB()
+    this.llenarTablaDB();
+    this.getDataPagina(1);
   },
   methods: {
     // logica de las tablas
     buscarPelicula: function () {
       fetch("https://www.omdbapi.com/?apikey=e85fe802&s=" + this.name)
         .then(async response => {
-          const data = await response.json()
-          this.peliculas = data["Search"]
+          const data = await response.json();
+          this.peliculas = data["Search"];
+          this.resultadoCantidad =this.peliculas.length; 
         })
         .catch(error => {
-          this.errorMessage = error
-          console.error('hay un error', error)
+          this.errorMessage = error;
+          console.error('hay un error', error);
         })
+
     },
     agregarPelicula: function () {
       fetch("routes/CreatePeliculas.php", {
@@ -41,8 +48,10 @@ new Vue({
         method: 'POST'
       })
         .then(async response => {
-          const data = await response.json()
-          this.peliculasDB = data
+          const data = await response.json();
+          this.peliculasDB = data;
+          this.resultadoCantidad = data.length;
+          console.log('data ', this.resultadoCantidad);
         }).catch(error => {
           console.log("Error ", error)
         })
@@ -56,8 +65,8 @@ new Vue({
         method: "POST",
         body: JSON.stringify(data)
       }).then(async response => {
-        const data = await response.json()
-          this.peliculasDB = data
+        const data = await response.json();
+          this.peliculasDB = data;
       }).catch(error => {
       })
     },
@@ -69,8 +78,8 @@ new Vue({
         method: "POST",
         body: JSON.stringify(data)
       }).then(async response => {
-        const data = await response.json()
-        this.peliculasDB = data
+        const data = await response.json();
+        this.peliculasDB = data;
       }).catch(error => {
 
       })
@@ -83,8 +92,8 @@ new Vue({
         method: 'POST',
         body: JSON.stringify(data)
       }).then(async response => {
-        const data = await response.json()
-        this.peliculasDB = data
+        const data = await response.json();
+        this.peliculasDB = data;
       }).catch(error => {
 
       })
@@ -97,11 +106,38 @@ new Vue({
         method: 'POST',
         body: JSON.stringify(data)
       }).then(async response => {
-        const data = await response.json()
-        this.peliculasDB = data
+        const data = await response.json();
+        this.peliculasDB = data;
       }).catch(error => {
 
       })
-    }
+    },
+    // Logica Paginado
+    totalPaginas() {
+			return Math.ceil(this.peliculas.length / this.elementosPorPagina);
+		},
+		getDataPagina(noPagina) {
+      console.log("data ", this.resultadoCantidad);
+			this.paginaActual = noPagina;
+			this.datosPaginados = [];
+			let ini = noPagina * this.elementosPorPagina - this.elementosPorPagina;
+			let fin = noPagina * this.elementosPorPagina;
+			this.datosPaginados = this.peliculas.slice(ini, fin);
+		},
+		getPreviousPage() {
+			if (this.paginaActual > 1) {
+				this.paginaActual--;
+			}
+			this.getDataPagina(this.paginaActual);
+		},
+		getNextPage() {
+			if (this.paginaActual < this.totalPaginas()) {
+				this.paginaActual++;
+			}
+			this.getDataPagina(this.paginaActual);
+		},
+		isActive(noPagina) {
+			return noPagina == this.paginaActual ? 'active' : '';
+		},
   }
 })
